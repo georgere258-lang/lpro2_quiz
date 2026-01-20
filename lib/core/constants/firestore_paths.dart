f5ec78a2b1d9ca263db51276a7e8740f7db03ffc
George Adel <george.re258@gmail.com>
2026-01-20 21:06:22 +0200

    chore: remove unused pro card collections paths

--- lib/core/constants/firestore_paths.dart
+++ lib/core/constants/firestore_paths.dart
@@ -5,9 +5,9 @@ class FirestorePaths {
   FirestorePaths._();
 
   // Pro Card CMS
-  static const String proCards = 'home_pro_cards';
+
   static const String proCardCurrent = 'home_pro_card';
-  static const String proCardsDeleted = 'home_pro_cards_deleted';
+
   static const String currentDoc = 'current';
   static const String history = 'history';
 }
