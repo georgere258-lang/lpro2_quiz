922bac4e2b1fab9a004ef794e4fd4ccb3ad04191
George Adel <george.re258@gmail.com>
2026-02-06 12:32:17 +0200

    chore(ios): bump version to 18 + minimal probe UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const TestApp());
+  runApp(const _ProbeApp());
 }
 
-class TestApp extends StatelessWidget {
-  const TestApp({super.key});
+class _ProbeApp extends StatelessWidget {
+  const _ProbeApp();
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
         body: Center(
           child: Text(
-            'HELLO IOS',
-            style: TextStyle(color: Colors.white, fontSize: 24),
+            'PROBE UI OK',
+            textDirection: TextDirection.ltr,
           ),
         ),
       ),
