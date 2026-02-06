95eb60478856b71223fe5df7c0db484b70b9952b
George Adel <george.re258@gmail.com>
2026-02-06 15:01:59 +0200

    Step 3: Hard-reset plugin registrant to empty state to bypass native Firebase crash

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  // تم تفريغ الإضافات يدوياً لكسر حلقة الشاشة السوداء في نسخة الاختبار رقم 22
+}
+
+@end
